6a8f881500ed01ac75c9e3bb1992a0eedcf4e801
Albin Baby <albinbabayme05@gmail.com>
2025-04-10 13:54:22 +0200

    cretaed class and executed the Calculate pay for Fulltime employee

---
+++ Employee Payroll System/FullTimeEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_System
+{
+    internal class FullTimeEmployee : Employee
+    {
+
+        public static double OverTimeBonous = 0.2;
+        
+        public int WorkedHours;
+        private double FullTimeEmployeeSalary;
+        public FullTimeEmployee(string name, string id, double salary, int workedHours)
+            :base(name, id, salary)
+        {
+            WorkedHours = workedHours;
+        }
+
+        public override double CalculatePay()
+        {
+            if(WorkedHours > 40)
+            {
+                return GetSalary() * (1 + OverTimeBonous);
+            }else if (WorkedHours <= 0)
+            {
+                Console.WriteLine("Working hours can not be Zero or Negtive");
+            }
+            return GetSalary();
+            
+        }
+    }
+}
---
+++ Employee Payroll System/PayRollManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_System
+{
+    internal class PayRollManager
+    {
+        private List<Employee> payReports = new List<Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            payReports.Add(employee);
+        }
+
+        public void showAllEmployee()
+        {
+            foreach (var employe in payReports)
+            {
+                Console.WriteLine($"Name: {employe.Name} | Id: {employe.GetId()} |" +
+                    $" Base Salary: {employe.GetSalary()} | Total: {employe.CalculatePay()} ");
+                Console.WriteLine("=========================================================");
+            }
+        }
+    }
+}
