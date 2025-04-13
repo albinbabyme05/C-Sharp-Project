62816eef7dcd0b41d447f56f5ef845122ae57fde
Albin Baby <albinbabayme05@gmail.com>
2025-04-14 00:10:00 +0200

    file modified - abstract class implimented

--- RentACar/Car.cs
+++ RentACar/Car.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace RentACar
 {
-    internal class Car
+    internal class Car : Vehicle
     {
+        private double BaseRate;
+        private double DailyRate;
+        public Car(string vehicleId, string model, string brand):base(vehicleId, model, brand)
+        {
+             BaseRate = 35.4;
+             DailyRate = 12.3;
+        }
+        public override double CalculateRent(int days)
+        {
+            return BaseRate + DailyRate;
+        }
     }
 }
