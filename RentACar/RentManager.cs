3d5a3454b5481dd1a768a92cb41081b672e0a716
Albin Baby <albinbabayme05@gmail.com>
2025-04-14 11:12:22 +0200

    method defined for this module, adding, check availbiltiy and display details

--- RentACar/RentManager.cs
+++ RentACar/RentManager.cs
@@ -7,6 +7,57 @@ using System.Threading.Tasks;
 namespace RentACar
 {
     internal class RentManager
+
     {
+        public bool IsAvailable=true;
+        private List<Vehicle> kart = new List<Vehicle>();
+
+        public void AddVehicle(Vehicle item)
+        {
+            kart.Add(item);
+        }
+
+       
+        public void RentedAVehicle(string vehicle_id)
+        {
+            foreach (var item in kart)
+            {
+                if (item.GetVehicleId() == vehicle_id)
+                {
+                    item.SetStatus("Rented");
+                    Console.WriteLine($"vechile {item.GetModel()} Id: {item.GetVehicleId()} Rented ");
+                    return;
+                }
+                
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            foreach (var item in kart)
+            {
+                if (item.GetStatus() == "Available")
+                {
+                    Console.WriteLine($"vechileId: {item.GetVehicleId()} - Brand: {item.GetBrand()}  - Model: {item.GetModel()} - Status: {item.GetStatus()} ");
+
+                }
+                else
+                {
+                    Console.WriteLine($"vechileId: {item.GetVehicleId()}  - Brand: {item.GetBrand()} - Model: {item.GetModel()} - Status: Rented ");
+                }
+            }
+        }
+
+        
+        //public void RentReport(string vechicleId)
+        //{
+        //    foreach (var item in kart)
+        //    {
+        //        if (item.GetVehicleId() == vechicleId)
+        //        {
+        //            Console.WriteLine($"vechile {item.GetModel()} Model: {item.GetModel()} Id: {item.GetVehicleId()} Status: {item.CalculateRent()} ");
+        //        }
+        //    }
+        //}
     }
 }
