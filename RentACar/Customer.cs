47eda302a296cfacd3ee0dad7d2fd8853fa032e6
Albin Baby <albinbabayme05@gmail.com>
2025-04-14 14:30:05 +0200

    customer tracking initzailed

---
+++ RentACar/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    internal class Customer
+    {
+        private string CustomerID;
+        private string Name;
+        private string ContactDetails;
+
+    }
+}
