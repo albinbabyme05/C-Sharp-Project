7fca4e7483823ef11e2db3aa74d40e1229215a9d
Albin Baby <albinbabayme05@gmail.com>
2025-04-14 22:31:58 +0200

    created a class that add 'customer' and 'vehicle' objects

---
+++ RentACar/RentalRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar
+{
+    internal class RentalRecord
+    {
+        public Customer Customer { get; set; }
+        public Vehicle Vehicle{ get; set; }
+
+        // constructor
+        public RentalRecord(Customer customer, Vehicle vehicle)
+        {
+            Customer = customer;
+            Vehicle = vehicle;
+        }
+
+        public string PrintReport()
+        {
+            return $"{Customer.GetName()} || {Customer.GetPhone()} || {Vehicle.GetVehicleId()} || {Vehicle.GetModel()} || " +
+                $" {Vehicle.CalculateRent(Vehicle.GetVehicleId())} Euro for {Vehicle.GetRentedDay()} Days ";
+        }
+    }
+}
