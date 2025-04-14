a8eedb59c87b4407e3dfbe0320944053ec5ccb90
Albin Baby <albinbabayme05@gmail.com>
2025-04-14 13:40:48 +0200

    file modified - removed WriteLine

--- RentACar/Truck.cs
+++ RentACar/Truck.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace RentACar
 {
-    internal class Truck
+    internal class Truck : Vehicle
     {
+        private double BaseRate;
+        private double DailyRate;
+        public Truck(string vehicleId, string model, string brand) : base(vehicleId, model, brand)
+        {
+            BaseRate = 60;
+            DailyRate = 20;
+        }
+        public override double CalculateRent(string vehicleId)
+        {
+
+            if (vehicleId == GetVehicleId() && GetStatus() == "Rented")
+            {
+                
+                return BaseRate + (DailyRate * GetRentedDay());
+            }
+            else
+            {
+                return 0.0;
+            }
+
+        }
     }
 }
