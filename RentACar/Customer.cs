84fd22b6c4babb8ac6f6cbc9c93a8bffe10931a8
Albin Baby <albinbabayme05@gmail.com>
2025-04-14 22:30:41 +0200

    Customer detail added fo tracking purpose

--- RentACar/Customer.cs
+++ RentACar/Customer.cs
@@ -8,9 +8,27 @@ namespace RentACar
 {
     internal class Customer
     {
-        private string CustomerID;
+        
         private string Name;
-        private string ContactDetails;
+        private string Phone;
+        private string CustomerId;
 
+        public Customer(string name, string customerId, string contact)
+        {
+            Name = name;
+            Phone = contact;
+            CustomerId = customerId;
+        }
+
+        public void SetName(string name) { Name = name;  }
+        public string GetName() => Name;
+        public void SetPhone(string phone) { Phone = phone; }
+        public string GetPhone() => Phone;
+
+
+        public void DisplayCustomer()
+        {
+            Console.WriteLine($"Name : {Name} - Id: {CustomerId} - Phone: {Phone}");
+        }
     }
 }
